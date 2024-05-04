720a06d95e1666c7564072e60e6c5918465536ec
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2024-05-04 20:14:50 -0300

    feat: github stats

--- next.config.js
+++ next.config.js
@@ -3,4 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github-readme-stats.vercel.app',
+        port: '',
+        pathname: '/?username=wellingtonengps&hide=html&layout=compact&theme=dracula',
+      },
+    ],
+  },
+}
+
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,30 +13,19 @@ export default function Home() {
                 <title>Wellington P. Silva</title>
                 <meta name="description" content="About Wellington P. Silva"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico"/>
+                <link rel="icon" href="favicon.ico"/>
             </Head>
             <main className={`${styles.main} ${inter.className}`}>
                 <div className={styles.header}>
                     <div className={styles.wrapperHeader}>
                         <div className={styles.wrapperProfileLinks}>
-                            <div>
-                                <Image
-                                    src="profile.svg"
-                                    alt="Profile"
-                                    width={245}
-                                    height={245}
-                                    priority
-                                />
-                            </div>
-                        </div>
-                        <div className={styles.description}>
-                            <h2 className={styles.title}>Wellington Pereira Silva</h2>
-                            <div className={styles.about}>
-                                <p>Olá, sou Wellington estudante de Ciência da Computação, estagiário em suporte de TI
-                                    no
-                                    Hospital Unimed Juiz de Fora, com experiência em desenvolvimento Web e mobile, com
-                                    aptidão também em UX/UI, buscando uma oportunidade como desenvolver.</p>
-                            </div>
+                            <Image
+                                src="profile.svg"
+                                alt="Profile"
+                                width={245}
+                                height={245}
+                                priority
+                            />
                             <div className={styles.links}>
                                 <a
                                     href="https://github.com/wellingtonengps"
@@ -54,62 +43,85 @@ export default function Home() {
                                     <Image className={styles.iconLink} src="linkedin.svg" alt="Github link" width={33}
                                            height={33} priority/>
                                 </a>
+
+                            </div>
+                        </div>
+                        <div className={styles.description}>
+                            <h1 className={styles.title}>Wellington Pereira Silva</h1>
+                            <div className={styles.about}>
+                                <p>Olá, sou Wellington estudante de Ciência da Computação, estagiário em suporte de TI
+                                    no
+                                    Hospital Unimed Juiz de Fora, com experiência em desenvolvimento Web e mobile, com
+                                    aptidão também em UX/UI, buscando uma oportunidade como desenvolver.</p>
+                            </div>
+
+                            <div className={styles.githubStats}>
+                                <img
+                                    src="https://github-readme-stats.vercel.app/api?username=wellingtonengps&theme=dracula"
+                                    alt="GitHub Stats"
+                                />
+                                <img
+                                    src="https://github-readme-stats.vercel.app/api/top-langs/?username=wellingtonengps&hide=html&layout=compact&theme=dracula"
+                                    alt="GitHub Stats"/>
                             </div>
                         </div>
                     </div>
                 </div>
 
 
-                <div className={styles.grid}>
-                    <div className={styles.card}>
-                        <h4>Liguagens de programação</h4>
-                        <div>
-                            <Image className={styles.iconLink} src="c.svg" alt="Github link" width={32}
-                                   height={32} priority/>
-                            <Image className={styles.iconLink} src="java.svg" alt="Github link" width={32}
-                                   height={32} priority/>
-                            <Image className={styles.iconLink} src="javascript.svg" alt="Github link" width={32}
-                                   height={32} priority/>
-                            <Image className={styles.iconLink} src="kotlin.svg" alt="Github link" width={32}
-                                   height={32} priority/>
+                <div className={styles.hardSkillsWrapper}>
+                    <h1 className={styles.subtitle}>Hard-Skills</h1>
+                    <div className={styles.grid}>
+                        <div className={styles.card}>
+                            <h4>Liguagens de programação</h4>
+                            <div>
+                                <Image className={styles.iconLink} src="c.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                                <Image className={styles.iconLink} src="java.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                                <Image className={styles.iconLink} src="javascript.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                                <Image className={styles.iconLink} src="kotlin.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className={styles.card}>
-                        <h4>Frameworks</h4>
-                        <div>
-                            <Image className={styles.iconLink} src="react.svg" alt="Github link" width={32}
-                                   height={32} priority/>
-                            <Image className={styles.iconLink} src="spring.svg" alt="Github link" width={32}
-                                   height={32} priority/>
-                            <Image className={styles.iconLink} src="next.svg" alt="Github link" width={32}
-                                   height={32} priority/>
-                            <Image className={styles.iconLink} src="nest.svg" alt="Github link" width={32}
-                                   height={32} priority/>
+                        <div className={styles.card}>
+                            <h4>Frameworks</h4>
+                            <div>
+                                <Image className={styles.iconLink} src="react.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                                <Image className={styles.iconLink} src="spring.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                                <Image className={styles.iconLink} src="next.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                                <Image className={styles.iconLink} src="nest.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className={styles.card}>
-                        <h4>Banco de dados</h4>
-                        <div>
-                            <Image className={styles.iconLink} src="mysql.svg" alt="Github link" width={32}
-                                   height={32} priority/>
-                            <Image className={styles.iconLink} src="postgresql.svg" alt="Github link" width={32}
-                                   height={32} priority/>
-                            <Image className={styles.iconLink} src="mongodb.svg" alt="Github link" width={32}
-                                   height={32} priority/>
+                        <div className={styles.card}>
+                            <h4>Banco de dados</h4>
+                            <div>
+                                <Image className={styles.iconLink} src="mysql.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                                <Image className={styles.iconLink} src="postgresql.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                                <Image className={styles.iconLink} src="mongodb.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className={styles.card}>
-                        <h4>Ferramentas</h4>
-                        <div>
-                            <Image className={styles.iconLink} src="git.svg" alt="Github link" width={32}
-                                   height={32} priority/>
-                            <Image className={styles.iconLink} src="docker.svg" alt="Github link" width={32}
-                                   height={32} priority/>
-                            <Image className={styles.iconLink} src="figma.svg" alt="Github link" width={32}
-                                   height={32} priority/>
+                        <div className={styles.card}>
+                            <h4>Ferramentas</h4>
+                            <div>
+                                <Image className={styles.iconLink} src="git.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                                <Image className={styles.iconLink} src="docker.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                                <Image className={styles.iconLink} src="figma.svg" alt="Github link" width={32}
+                                       height={32} priority/>
+                            </div>
                         </div>
                     </div>
                 </div>
